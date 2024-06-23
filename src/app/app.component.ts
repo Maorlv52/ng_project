2d0efe9a88766ef034adf11ad006bac3a8ff2a11
Maor.Levinshtein <maor.levinshtein@payboxapp.com>
2024-06-23 13:11:12 +0300

    new angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-course';
+  title = 'Maor`s app'
+  name = 'Maor'
 }
